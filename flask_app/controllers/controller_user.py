8fab2e167b7dfab1516fed99c9a4fa415e434902
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-05-16 23:07:02 -0700

    updates

--- flask_app/controllers/controller_user.py
+++ flask_app/controllers/controller_user.py
@@ -3,10 +3,20 @@ from flask_app import app
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
+# remember to install bcrypt & flask
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/user/login', methods= ['post'])    
+def login():
+    is_valid = User.is_valid(request.form)
+
+    if not is_valid:
+        return redirect('/')
+
+    return redirect('/')
 
 @app.route('/register',methods=['POST'])
 def save():
@@ -19,6 +29,7 @@ def save():
         "email": request.form['email'],
         "password": bcrypt.generate_password_hash(request.form['password'])
     }
+    # forgot to do this. remember to add to db
     id = User.save(data)
     session['user_id'] = id
 
